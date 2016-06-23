a458366cd8b97fa02906197afbd57d110a0d380a
Brandon Lilly <brandonjlilly@gmail.com>
2016-06-22 17:41:35 -0700

    webpack 2, webpack validator, package scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,47 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
-var config = {
-  devtool : 'eval-source-map',
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractSASS = new ExtractTextPlugin('styles.css');
+
+const config = env => ({
+  devtool: env.prod ? undefined : 'eval-source-map',
 
   entry: [
     './app/index.js',
     './style/main.scss',
   ],
+
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'dist/generated'),
+    path: path.resolve(__dirname, 'dist/generated'),
     publicPath: '/',
   },
+
   resolve: {
     extensions: ['', '.js', '.jsx', '.scss', '.css'],
     root: [ path.join(__dirname, 'app/') ],
   },
+
   module: {
     loaders: [
-      {
-        test: /\.js$/, loader: 'babel', exclude: /node_modules/,
-        query: { presets: ['es2015', 'react', 'stage-0'] }
-      },
-      {
-        test: /\.scss$/,
-        loaders: ['style', 'css?sourceMap', 'sass?sourceMap'],
-      },
+      { test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: { presets: ['es2015-webpack', 'stage-2'] } },
+      { test: /\.scss$/,
+        loader: extractSASS.extract('style-loader', 'css!sass') },
     ]
   },
-  sassLoader: {
-    includePaths: [path.join(__dirname, "style")]
-  }
-};
+
+  plugins: [
+    extractSASS,
+  ].concat(!env.prod ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.LoaderOptionsPlugin({ minimize: true, debug: false }),
+    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"production"' } }),
+    new webpack.optimize.UglifyJsPlugin({ compress: { screw_ie8: true, warnings: false }}),
+  ]),
+});
 
 module.exports = config;
